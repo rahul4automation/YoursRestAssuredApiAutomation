65f17c1e3a46c50eb78f4567f384a60cf5e46f54
RahulKumar <rahul4automation@gmail.com>
2023-01-12 18:52:44 +0530

    To run test cases using TestNG.xml

--- src/test/java/airLine/AirLineTests.java
+++ src/test/java/airLine/AirLineTests.java
@@ -18,14 +18,14 @@ public class AirLineTests {
     public void createAirLine() {
         Response response = AirLineService.createCustomerAirLines(APIEndPoints.METHOD_POST_CREATE_AIRLINES,
                 Payloads.postCreateAirLinePayload("111299", "Sri Lankan Airways", "Sri Lanka", "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png",
-                        "From Sri Lanka", "Katunayake, Sri Lanka", "www.srilankaairways.com", "1990"), new HashMap<>());
+                        "From Sri Lanka", "Katunayake, Sri Lanka", "www.srilankaairways.com", "19999"), new HashMap<>());
         Assert.assertEquals(response.statusCode(), 200);
     }
 
     @Test
     public void createAirLineUsingMap() {
         Response response = AirLineService.createPostCustomerAirLines(APIEndPoints.METHOD_POST_CREATE_AIRLINES, Payloads.postCreateAirLineUsingMap("9822", "Sri Lankan Airways", "Sri Lanka", "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png",
-                "From Sri Lanka", "Katunayake, Sri Lanka", "www.srilankaairways.com", "1990"), new HashMap<>());
+                "From Sri Lanka", "Katunayake, Sri Lanka", "www.srilankaairways.com", "19977"), new HashMap<>());
         Assert.assertEquals(response.statusCode(), 200);
     }
 
@@ -35,7 +35,7 @@ public class AirLineTests {
         String env = System.getProperty("env") == null ? "qa" : System.getProperty("env");
         Map <String, String> data= JsonUtils.getJsonDataAsMap("airLines/qa/airLinesApiData.json");
         String endPoint=data.get("CreateAirLineApiEndPoint");
-        Response response = AirLineService.createPostCustomerAirLines(endPoint, Payloads.postCreateAirLineUsingMap("982", "Sri Lankan Airways", "Sri Lanka", "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png",
+        Response response = AirLineService.createPostCustomerAirLines(endPoint, Payloads.postCreateAirLineUsingMap("98892", "Sri Lankan Airways", "Sri Lanka", "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png",
                 "From Sri Lanka", "Katunayake, Sri Lanka", "www.srilankaairways.com", "1990"), new HashMap<>());
         Assert.assertEquals(response.statusCode(), 200);
     }
